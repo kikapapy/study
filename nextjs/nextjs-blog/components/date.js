3836e2b3af1101a2d368c8cfe0ccb71202bc2000
kika <kikapapy@gmail.com>
2023-02-18 22:33:02 +0900

    :triangular_flag_on_post: CREATE YOUR FIRST APP - Dynamic Routes

---
+++ nextjs/nextjs-blog/components/date.js
@@ -0,0 +1,6 @@
+import { parseISO, format } from "date-fns";
+
+export default function Date({ dateString }) {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, "yyyy년 LL월 dd일")}</time>;
+}
